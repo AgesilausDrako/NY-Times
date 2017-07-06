829f31fe8505dc198a3e967dc2497477624b8312
Terri Pannell <Terri Pannell>
2017-07-05 23:04:13 -0400

    Fixed issue with getting input from form

--- assets/javascript/clay.js
+++ assets/javascript/clay.js
@@ -1,38 +1,40 @@
 $(document).ready(function(){
-	var search = $("#search");
-	var limit = $("#records");
-	var yearB = $("#start");
-	var yearE = $("#end");
 
-	$("#button-search").click(function(){
 
-	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-		if (yearB == null || yearE == null) {
+	$("#search-button").click(function(){
+		var search = $("#search").val().trim();
+		console.log(search);
+		var limit = $("#records").val().trim();
+		console.log(limit);
+		var yearB = $("#start").val().trim();
+		console.log(yearB);
+		var yearE = $("#end").val().trim();
+		console.log(yearE);
+		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+		if (yearB.length < 4 || yearE.length < 4) {
 			url += '?' + $.param({
 			  'api-key': "d458e2def8dc4f8dbd1a70891929e0f2",
 			  'q': search,
-			  'page': limit/10-1
+			  'page': limit
 			});
 		} else {
 			url += '?' + $.param({
 			  'api-key': "d458e2def8dc4f8dbd1a70891929e0f2",
 			  'q': search,
-			  'page': limit/10-1,
+			  'page': 1,
 			  'begin_date': yearB,
 			  'end_date': yearE
 			});
 		}
-			$.ajax({
+		console.log(url);
+
+		$.ajax({
 			  url: url,
 			  method: 'GET',
 			}).done(function(result) {
 			  console.log(result);
-			}).fail(function(err) {
-			  throw err;
-		});
-
-		
-			var results = response.docs;
+			var results = result.docs;
 
 			for (i=0; i<results.length; i++) {
 				var articleDiv = $("<div>");
@@ -47,7 +49,12 @@ $(document).ready(function(){
 
 			}
 
-			$("#topArticles").prepend(articleDiv);
+			$("#topArticles").prepend(articleDiv);	
+					
+		});
+
+		
+
 		});//click end
 
 		$("#clear").click(function(){
